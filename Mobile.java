6dc5a0b589ecfb04f486606a8ef927394fa63084
Deepika G <124426811+dpg93@users.noreply.github.com>
2023-02-04 23:12:28 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "Apple";
+		System.out.println(mobileBrandName);
+		char mobileLogo = '@';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece = 7;
+		System.out.println(noOfMobilePiece);
+		int ModelNumber = 12;
+		System.out.println(ModelNumber);
+		long mobileImeiNumber = 987654321012l;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice = 64000f;
+		System.out.println(mobilePrice);
+		boolean isDamaged = false;
+		System.out.println(isDamaged);
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels = 2;
+	System.out.println(noOfWheels);
+	short noOfMirrors = 2;
+	System.out.println(noOfMirrors);
+	long chassisNumber = 234567997653366777l;
+	System.out.println(chassisNumber);
+	boolean isPunctured = false;
+	System.out.println(isPunctured);
+	String bikeName = "Vespa";
+	System.out.println(bikeName);
+	double runningKM = 390.8;
+	System.out.println(runningKM);
+}
+}
